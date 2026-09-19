18b47647175bf98a84558ea7889f420d514b3f28
sparky <aryan2008raj00@gmail.com>
2026-09-19 19:56:06 +0530

    second

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forr(i,t) for(int i = 0; i < t; i++)
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello World";
+
+    return 0;
+}
